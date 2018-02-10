021c92d042b3a5f53fdaa6973a619b55b8871616
Azamat <36338246+hypnojaaab@users.noreply.github.com>
2018-02-10 23:07:02 +0300

    пробная загрузка для проверки команд

---
+++ js/js.js
@@ -0,0 +1,11 @@
+
+var Tc = 1; // температура по цельцию
+var Tf = (9 / 5) * Tc + 32; // температура по фаренгейту
+alert(Tf);
+
+var admin;
+var name = "Василий";
+admin = name;
+alert(admin);
+
+alert(1000 + "108");
